3def1bb83a5954bcbe86037ccb6fce0c53a025a1
Matteo Taroli <taroli.matteo@gmail.com>
2015-11-10 12:48:49 +0100

    Add SplashActivity

---
+++ app/src/main/java/be/ipl/mobile/projet/jeudepiste/ListeEpreuveActivity.java
@@ -0,0 +1,18 @@
+package be.ipl.mobile.projet.jeudepiste;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by matt on 10/11/15.
+ */
+public class ListeEpreuveActivity extends AppCompatActivity {
+
+    public static final String EXTRA_ETAPE = "etape";
+    public static final String EXTRA_EPREUVE = "epreuve";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
