577ddba313521f5c7ded6e9d8deb3f6f31401597
Matteo Taroli <taroli.matteo@gmail.com>
2015-11-22 01:25:45 +0100

    Ajoute License et javadoc à EtapeActivity
    Oups, étrangement oublié lors du commit précédent

--- app/src/main/java/be/ipl/mobile/projet/historypub/EtapeActivity.java
+++ app/src/main/java/be/ipl/mobile/projet/historypub/EtapeActivity.java
@@ -1,3 +1,22 @@
+/*
+    Copyright (C) 2015
+        Matteo Taroli <contact@matteotaroli.be>
+        Nathan Raspe <raspe_nathan@live.be>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package be.ipl.mobile.projet.historypub;
 
 import android.content.Context;
@@ -20,7 +39,7 @@ import be.ipl.mobile.projet.historypub.pojo.Etape;
 import be.ipl.mobile.projet.historypub.pojo.epreuves.Type;
 
 /**
- * Created by matt on 19/11/15.
+ * Activité présentant une étape du jeu à l'utilisateur.
  */
 public class EtapeActivity extends AppCompatActivity {
     private static final String TAG = "EtapeActivity";
@@ -39,7 +58,7 @@ public class EtapeActivity extends AppCompatActivity {
 
         mWebView = (WebView) findViewById(R.id.webView);
         GestionEtapes gestionEtapes = GestionEtapes.getInstance(this);
-        mEtape = gestionEtapes.getEtape(getIntent().getIntExtra(Config.EXTRA_ETAPE_COURANTE, 0));
+        mEtape = gestionEtapes.getEtape(getIntent().getIntExtra(Config.EXTRA_ETAPE, 0));
 
         Log.d(TAG, "chargement de : " + mEtape.getUrl());
 
@@ -66,6 +85,9 @@ public class EtapeActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * Charge le fichier HTML dans la webview et défini la gestion des liens.
+     */
     private void setupWebview() {
         mWebView.setWebViewClient(new WebViewClient() {
             @Override
@@ -77,6 +99,11 @@ public class EtapeActivity extends AppCompatActivity {
         mWebView.loadUrl("file:///android_asset/" + getString(R.string.prefix) + File.separator + mEtape.getUrl());
     }
 
+    /**
+     * Lance l'épreuve correspondante au lien URL donné.
+     *
+     * @param url URL correspondant à l'épreuve
+     */
     private void lanceEpreuveCorrespondante(String url) {
         Log.i("ET", mEtape.toString());
         Log.i("NB_EP", mEtape.getNombreEpreuves() + "");
@@ -97,7 +124,7 @@ public class EtapeActivity extends AppCompatActivity {
                     .putLong(Config.PREF_TEMPS_DEBUT, new Date().getTime())
                     .apply();
         }
-        intent.putExtra(Config.EXTRA_ETAPE_COURANTE, mEtape.getNum());
+        intent.putExtra(Config.EXTRA_ETAPE, mEtape.getNum());
         intent.putExtra(Config.EXTRA_EPREUVE, mEtape.getEpreuve(url).getUri());
 
         Log.d(TAG, "Url capturée : " + url);
